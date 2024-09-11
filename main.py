8f42f0e62f987228d13951b31fd98356d9e340dc
Jürg Käser <juerg.kaeser@abaqon.com>
2024-09-12 00:22:05 +0200

    changed file name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from utils import logger
-from services import account
-from services import notification, automation
+from services import account, monitoring
+from services import automation
 
 logger.setup_logger()
 
 acc = account.Account()
-ntf = notification.Notificaton()
+ntf = monitoring.Notificaton()
 atm = automation.Automation()
 
 login_bonus = acc.get_login_bonus()
